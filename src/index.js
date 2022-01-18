91ceaa2cdc797d4b7c9d5c7aa2624cb70b816963
Alison Melendez <alisonmelendez132@gmail.com>
2022-01-17 19:16:44 -0500

    Elements adding onto to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+
+        e.preventDefault();
+
+        let inputtedValue = e.target.newTask.value
+        console.log(inputtedValue)
+        grabTodo(inputtedValue)
+
+    })
+
+    function grabTodo(todo) {
+        let li = document.createElement('li')
+        let btn = document.createElement('button')
+        btn.textContent = 'x' // button for later to delete
+        li.textContent = todo
+        let list = document.querySelector('#tasks')
+        list.appendChild(li)
+
+    }
+
 });
